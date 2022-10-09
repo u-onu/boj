f4f06f43e0095dff48a1d5c9f060a718e2bf0fbc
u-onu <uon.sleeping@gmail.com>
2022-10-09 14:39:16 +0900

    Hello World

--- boj_2557.cpp
+++ boj_2557.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    freopen("input.txt", "r", stdin);
     cout << "Hello World!\n";
 }
