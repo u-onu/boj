2a072cdcb8f754ecc1a4c206fcd253fb15e47653
u-onu <uon.sleeping@gmail.com>
2022-10-09 14:32:32 +0900

    boj_11654

---
+++ boj_11654.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    freopen("input.txt", "r", stdin);
+
+    char input;
+    cin >> input;
+    cout << int(input) << '\n';
+}
