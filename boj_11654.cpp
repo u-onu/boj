173ef74bad8fb3c366ba3974aa21a8b319e8bd5c
u-onu <uon.sleeping@gmail.com>
2022-10-09 14:52:21 +0900

    아스키 코드

--- boj_11654.cpp
+++ boj_11654.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
-    freopen("input.txt", "r", stdin);
+    // freopen("input.txt", "r", stdin);
 
     char input;
     cin >> input;
