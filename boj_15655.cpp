e2e66aea6d17c92d040c1123ee96365c604d1b38
u-onu <uon.sleeping@gmail.com>
2022-10-11 00:02:06 +0900

    N과 M (6)

---
+++ boj_15655.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+bool isused[10];
+int arr[10];
+
+void dfs(int depth, int index, vector<int> &v){
+  if(depth == m){
+    for(int i = 0; i < m; i++)
+      cout << arr[i] << ' ';
+    cout << '\n';
+    return;
+  }
+
+  for(int i = index; i < n; i++){
+    if(!isused[i]){
+      arr[depth] = v[i];
+      isused[i] = true;
+      dfs(depth + 1, i, v);
+      isused[i] = false;
+    }
+  }
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+  // freopen("input.txt", "r", stdin);
+
+  cin >> n >> m;
+  vector<int> input(n);
+  for(int i = 0; i < n; i++)
+    cin >> input[i];
+
+  sort(input.begin(), input.end());
+  dfs(0, 0, input);
+}
