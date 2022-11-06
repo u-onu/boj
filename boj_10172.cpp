a1e426bf432a393bae1741b2aea9dbe315b1c778
u-onu <uon.sleeping@gmail.com>
2022-11-06 22:04:46 +0900

    개

---
+++ boj_10172.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  string dog = 
+    "|\\_/|\n"
+    "|q p|   /}\n"
+    "( 0 )\"\"\"\\\n"
+    "|\"^\"`    |\n"
+    "||_/=\\\\__|";
+  
+  cout << dog;
+}
