52a0b27423552216ad0643178c7aedb6c347fcc2
u-onu <uon.sleeping@gmail.com>
2022-11-06 15:10:33 +0900

    사칙연산

---
+++ boj_10869.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int a, b;
+  cin >> a >> b;
+
+  cout << a + b << '\n';
+  cout << a - b << '\n';
+  cout << a * b << '\n';
+  cout << a / b << '\n';
+  cout << a % b << '\n';
+}
