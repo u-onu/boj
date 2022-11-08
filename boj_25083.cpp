13700dcf412c294344161ba8f5f9af33b822e916
u-onu <uon.sleeping@gmail.com>
2022-11-08 20:35:27 +0900

    새싹

---
+++ boj_25083.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  string sprout = 
+    "         ,r\'\"7\n"
+    "r`-_   ,\'  ,/\n"
+    " \\. \". L_r\'\n"
+    "   `~\\/\n"
+    "      |\n"
+    "      |\n";
+
+  cout << sprout;
+}
