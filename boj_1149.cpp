a8ec05ec54ebad593a5300608909812c5aac532b
u-onu <uon.sleeping@gmail.com>
2022-11-08 21:11:34 +0900

    RGB거리

--- boj_1149.cpp
+++ boj_1149.cpp
@@ -13,7 +13,7 @@ int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr); cout.tie(nullptr);
 
-   int n; cin >> n;
+  int n; cin >> n;
 
   int cost_R, cost_G, cost_B;
   for(int i = 1; i <= n; i++){
