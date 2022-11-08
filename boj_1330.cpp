042b7aeefd57ff96f19d7975e78db946f81aed5a
u-onu <uon.sleeping@gmail.com>
2022-11-08 20:34:04 +0900

    두 수 비교하기

---
+++ boj_1330.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int a, b;
+  cin >> a >> b;
+
+  if(a > b) cout << ">" << '\n';
+  else if(a < b) cout << "<" << '\n';
+  else cout << "==" << '\n';
+}
