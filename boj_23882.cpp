2799795ade3570061d86d52465b9446cf6e6e1c6
u-onu <uon.sleeping@gmail.com>
2022-11-13 11:00:28 +0900

    알고리즘 수업 - 선택 정렬 2

---
+++ boj_23882.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#define MAX_N 10000
+using namespace std;
+
+int arr[MAX_N];
+int n, k;
+
+void init(){
+  cin >> n >> k;
+  for(int i = 0; i < n; i++)
+    cin >> arr[i];
+}
+
+void print_arr(){
+  for(int i = 0; i < n; i++)
+    cout << arr[i] << ' ';
+  cout << '\n';
+}
+
+void selection_sort(){
+  int swap_cnt = 0;
+
+  for(int last = n - 1; last > 0; last--){
+    int max_num, max_pos;
+
+    max_num = max_pos = 0;
+    for(int i = 0; i <= last; i++)
+      if(arr[i] > max_num){
+        max_num = arr[i];
+        max_pos = i;
+      }
+
+    if(last != max_pos){
+      arr[max_pos] = arr[last];
+      arr[last] = max_num;
+      swap_cnt++;
+
+      if(swap_cnt == k){
+        print_arr();
+        return;
+      }
+    }
+  }
+
+  cout << -1 << '\n'; // swap_cnt is less than k.
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+  
+  init();
+  selection_sort();
+}
